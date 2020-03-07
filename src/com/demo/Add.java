9ca58171093866ea2599f04a215b555d3ca0b9ff
unknown <akleshsakhare@yahoo.com>
2020-03-07 09:13:16 +0530

    servletconfig and servletcontext

--- src/com/demo/Add.java
+++ src/com/demo/Add.java
@@ -3,6 +3,8 @@ package com.demo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -36,9 +38,14 @@ public class Add extends HttpServlet {
 		/*
 		 * request redirect will show redirected servlet in url in order to same request
 		 * data we need to use HttpSession or Cookie There are 3 ways of doing redirect
+		 * 
+		 * 
 		 * 1.URL rewritting-- disadvantavce-- if we want to pass value to different
-		 * servlet then not possible 2.HttpSession (Interface)-- basically used for
-		 * loginuser validation 3.Cookie(class)-- can be use to store multiple cookie
+		 * servlet then not possible
+		 * 
+		 * 2.HttpSession (Interface)-- basically used for loginuser validation
+		 * 
+		 * 3.Cookie(class)-- can be use to store multiple cookie
 		 */
 
 		// URL rewritting data
@@ -53,6 +60,20 @@ public class Add extends HttpServlet {
 		Cookie cookie = new Cookie("sum", sum + "");
 		response.addCookie(cookie);
 		response.sendRedirect("cookieDemo");
+		/*
+		 * ServletContext----> if we want to share same initial value to all servlet
+		 * then we should use servlet context
+		 * 
+		 * ServletConfig---->if we want to use differnt initial value for servlet then
+		 * we should use servlet config
+		 */
 
+		ServletContext context = getServletContext();
+		String valueOfServletContext = context.getInitParameter("servletInit");
+		System.out.println("valueOfServletContext = " + valueOfServletContext);
+
+		ServletConfig config = getServletConfig();
+		String valueOfServletConfig = config.getInitParameter("servletInit");
+		System.out.println("valueOfServletConfig = " + valueOfServletConfig);
 	}
 }
